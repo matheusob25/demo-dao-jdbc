b93741b78f14857fea666925270730e7511f6b19
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-04-04 13:53:54 -0300

    método findByDepartment

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,10 +5,19 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = sellerDao.findById(1);
         System.out.println(seller);
+
+        System.out.println("\n\n=============== FindByDepartment ================");
+        List<Seller> list = sellerDao.findByDepartment(new Department(1, null));
+        for (Seller s : list) {
+            System.out.println(s);
+        }
     }
 }
